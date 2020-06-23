a1689676bf63c8e264b61fd314afbf1d3a3188e8
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-23 03:34:59 +0300

    base: Allow hiding VoWiFi StatusBar icon [1/2]
    Stallix edits: Use Evo's current ContentObserver and ContentResolver formats and not use SystemUI Tuner
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6145,6 +6145,15 @@ public final class Settings {
         public static final Validator VOLTE_ICON_STYLE_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether to show VoWiFi icon or not
+         * @hide
+         */
+        public static final String VOWIFI_ICON = "vowifi_icon";
+
+        /** @hide */
+        public static final Validator VOWIFI_ICON_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Whether to blink flashlight for incoming calls
          * 0 = Disabled (Default)
          * 1 = Blink flashlight only in Ringer mode
@@ -7578,7 +7587,8 @@ public final class Settings {
             VOLUME_BUTTON_MUSIC_CONTROL,
             VOLUME_KEY_CURSOR_CONTROL,
             VOLUME_ROCKER_WAKE,
-            QS_DATAUSAGE
+            QS_DATAUSAGE,
+            VOWIFI_ICON,
             // Evolution X Settings end
         };
 
@@ -7866,6 +7876,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
             PRIVATE_SETTINGS.add(VOLUME_ROCKER_WAKE);
             PRIVATE_SETTINGS.add(QS_DATAUSAGE);
+            PRIVATE_SETTINGS.add(VOWIFI_ICON);
             // Evolution X Settings end
         }
 
@@ -8183,6 +8194,7 @@ public final class Settings {
             VALIDATORS.put(VOLUME_KEY_CURSOR_CONTROL, VOLUME_KEY_CURSOR_CONTROL_VALIDATOR);
             VALIDATORS.put(VOLUME_ROCKER_WAKE, VOLUME_ROCKER_WAKE_VALIDATOR);
             VALIDATORS.put(QS_DATAUSAGE, QS_DATAUSAGE_VALIDATOR);
+            VALIDATORS.put(VOWIFI_ICON, VOWIFI_ICON_VALIDATOR);
             // Evolution X Settings end
         }
 
