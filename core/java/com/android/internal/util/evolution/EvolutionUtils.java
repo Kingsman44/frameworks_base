7bcd167f4cff86e201168983285e15c4473c6e2d
AnkitGourav <Ankitgourav035@gmail.com>
2020-07-03 10:26:38 +0000

    Utils: add method to check if device is connected to Wi-Fi

--- core/java/com/android/internal/util/evolution/EvolutionUtils.java
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -120,6 +120,15 @@ public class EvolutionUtils {
         return wifi.isConnected() || mobile.isConnected();
     }
 
+    // Check if device is connected to Wi-Fi
+    public static boolean isWiFiConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) return false;
+
+        NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return wifi.isConnected();
+    }
+
     // Check to see if a package is installed
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
